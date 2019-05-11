7d507c6c8fb353ce18f7f0ecbfebee07794ae82d
Min Kang <kangm914@gmail.com>
2019-05-11 21:59:34 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -9,22 +9,9 @@ def position_taken?(board, index)
 end
 
 def valid_move?(board, index)
-  if board[index] == empty && board[index] == 
+  if board[index] == empty
+    true
   else position_taken? == true || index.between(0, 8)
-    nil 
-end
-
-  it 'returns nil or false for an occupied position' do
-    board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
-    index = 4
-
-    expect(valid_move?(board, index)).to be_falsey
-  end
-
-  it 'returns nil or false for a position that is not on the board' do
-    board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-    index = 100
-
-    expect(valid_move?(board, index)).to be_falsey
+    nil
   end
 end
